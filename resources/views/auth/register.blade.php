b2a2f2cf1d7fa65168f44f7f032518301ee3d8b9
unknown <dedoa82.as@gmail.com>
2022-04-20 00:49:06 +0200

    Authentication Architecture :: Fedration step 3

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -225,90 +225,10 @@ License: For each use you must have a valid license purchased only from above li
 								</div>
 								<!--end::Step 1-->
 								<!--begin::Step 2-->
-								@include('inc.auth.register-step-2')
+								@include('inc.auth.register-step-3')
 								<!--end::Step 2-->
 								<!--begin::Step 3-->
-								<div class="hide-if-player" data-kt-stepper-element="content">
-									<!--begin::Wrapper-->
-									<div class="w-100">
-										<!--begin::Heading-->
-										<div class="pb-10 pb-lg-12">
-											<!--begin::Title-->
-											<h2 class="fw-bolder text-dark">Business Details</h2>
-											<!--end::Title-->
-											<!--begin::Notice-->
-											<div class="text-muted fw-bold fs-6">If you need more info, please check out
-											<a href="#" class="link-primary fw-bolder">Help Page</a>.</div>
-											<!--end::Notice-->
-										</div>
-										<!--end::Heading-->
-										<!--begin::Input group-->
-										<div class="fv-row mb-10">
-											<!--begin::Label-->
-											<label class="form-label required">Business Name</label>
-											<!--end::Label-->
-											<!--begin::Input-->
-											<input name="business_name" class="form-control form-control-lg form-control-solid" value="Keenthemes Inc." />
-											<!--end::Input-->
-										</div>
-										<!--end::Input group-->
-										<!--begin::Input group-->
-										<div class="fv-row mb-10">
-											<!--begin::Label-->
-											<label class="d-flex align-items-center form-label">
-												<span class="required">Shortened Descriptor</span>
-												<i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="popover" data-bs-trigger="hover" data-bs-html="true" data-bs-content="&lt;div class='p-4 rounded bg-light'&gt; &lt;div class='d-flex flex-stack text-muted mb-4'&gt; &lt;i class='fas fa-university fs-3 me-3'&gt;&lt;/i&gt; &lt;div class='fw-bold'&gt;INCBANK **** 1245 STATEMENT&lt;/div&gt; &lt;/div&gt; &lt;div class='d-flex flex-stack fw-bold text-gray-600'&gt; &lt;div&gt;Amount&lt;/div&gt; &lt;div&gt;Transaction&lt;/div&gt; &lt;/div&gt; &lt;div class='separator separator-dashed my-2'&gt;&lt;/div&gt; &lt;div class='d-flex flex-stack text-dark fw-bolder mb-2'&gt; &lt;div&gt;USD345.00&lt;/div&gt; &lt;div&gt;KEENTHEMES*&lt;/div&gt; &lt;/div&gt; &lt;div class='d-flex flex-stack text-muted mb-2'&gt; &lt;div&gt;USD75.00&lt;/div&gt; &lt;div&gt;Hosting fee&lt;/div&gt; &lt;/div&gt; &lt;div class='d-flex flex-stack text-muted'&gt; &lt;div&gt;USD3,950.00&lt;/div&gt; &lt;div&gt;Payrol&lt;/div&gt; &lt;/div&gt; &lt;/div&gt;"></i>
-											</label>
-											<!--end::Label-->
-											<!--begin::Input-->
-											<input name="business_descriptor" class="form-control form-control-lg form-control-solid" value="KEENTHEMES" />
-											<!--end::Input-->
-											<!--begin::Hint-->
-											<div class="form-text">Customers will see this shortened version of your statement descriptor</div>
-											<!--end::Hint-->
-										</div>
-										<!--end::Input group-->
-										<!--begin::Input group-->
-										<div class="fv-row mb-10">
-											<!--begin::Label-->
-											<label class="form-label required">Corporation Type</label>
-											<!--end::Label-->
-											<!--begin::Input-->
-											<select name="business_type" class="form-select form-select-lg form-select-solid" data-control="select2" data-placeholder="Select..." data-allow-clear="true" data-hide-search="true">
-												<option></option>
-												<option value="1">S Corporation</option>
-												<option value="1">C Corporation</option>
-												<option value="2">Sole Proprietorship</option>
-												<option value="3">Non-profit</option>
-												<option value="4">Limited Liability</option>
-												<option value="5">General Partnership</option>
-											</select>
-											<!--end::Input-->
-										</div>
-										<!--end::Input group-->
-										<!--begin::Input group-->
-										<div class="fv-row mb-10">
-											<!--end::Label-->
-											<label class="form-label">Business Description</label>
-											<!--end::Label-->
-											<!--begin::Input-->
-											<textarea name="business_description" class="form-control form-control-lg form-control-solid" rows="3"></textarea>
-											<!--end::Input-->
-										</div>
-										<!--end::Input group-->
-										<!--begin::Input group-->
-										<div class="fv-row mb-0">
-											<!--begin::Label-->
-											<label class="fs-6 fw-bold form-label required">Contact Email</label>
-											<!--end::Label-->
-											<!--begin::Input-->
-											<input name="business_email" class="form-control form-control-lg form-control-solid" value="corp@support.com" />
-											<!--end::Input-->
-										</div>
-										<!--end::Input group-->
-									</div>
-									<!--end::Wrapper-->
-								</div>
+								@include('inc.auth.register-step-3')
 								<!--end::Step 3-->
 								<!--begin::Step 5-->
 								<div class="last-step" data-kt-stepper-element="content">
