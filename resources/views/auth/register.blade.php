cbdd9e33e2d14c5e5b97bdc6cd9c9d02ff015c59
unknown <dedoa82.as@gmail.com>
2022-04-20 02:04:49 +0200

    Authentication Architecture :: Fedration step 4

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -87,8 +87,8 @@ License: For each use you must have a valid license purchased only from above li
 									<!--end::Icon-->
 									<!--begin::Label-->
 									<div class="stepper-label">
-										<h3 class="stepper-title">Account Settings</h3>
-										<div class="stepper-desc fw-bold">Setup Your Account Settings</div>
+										<h3 class="stepper-title">Business Info</h3>
+										<div class="stepper-desc fw-bold">Your Business Related Info</div>
 									</div>
 									<!--end::Label-->
 								</div>
@@ -106,8 +106,8 @@ License: For each use you must have a valid license purchased only from above li
 									<!--end::Icon-->
 									<!--begin::Label-->
 									<div class="stepper-label">
-										<h3 class="stepper-title">Business Info</h3>
-										<div class="stepper-desc fw-bold">Your Business Related Info</div>
+										<h3 class="stepper-title">Account Settings</h3>
+										<div class="stepper-desc fw-bold">Setup Your Account Settings</div>
 									</div>
 									<!--end::Label-->
 								</div>
@@ -120,7 +120,7 @@ License: For each use you must have a valid license purchased only from above li
 									<!--begin::Icon-->
 									<div class="stepper-icon w-40px h-40px">
 										<i class="stepper-check fas fa-check"></i>
-										<span class="stepper-number">5</span>
+										<span class="stepper-number">4</span>
 									</div>
 									<!--end::Icon-->
 									<!--begin::Label-->
@@ -225,64 +225,13 @@ License: For each use you must have a valid license purchased only from above li
 								</div>
 								<!--end::Step 1-->
 								<!--begin::Step 2-->
-								@include('inc.auth.register-step-3')
+								@include('inc.auth.register-step-2')
 								<!--end::Step 2-->
 								<!--begin::Step 3-->
 								@include('inc.auth.register-step-3')
 								<!--end::Step 3-->
 								<!--begin::Step 5-->
-								<div class="last-step" data-kt-stepper-element="content">
-									<!--begin::Wrapper-->
-									<div class="w-100">
-										<!--begin::Heading-->
-										<div class="pb-8 pb-lg-10">
-											<!--begin::Title-->
-											<h2 class="fw-bolder text-dark">Your Are Done!</h2>
-											<!--end::Title-->
-											<!--begin::Notice-->
-											<div class="text-muted fw-bold fs-6">If you need more info, please
-											<a href="../../demo1/dist/authentication/sign-in/basic.html" class="link-primary fw-bolder">Sign In</a>.</div>
-											<!--end::Notice-->
-										</div>
-										<!--end::Heading-->
-										<!--begin::Body-->
-										<div class="mb-0">
-											<!--begin::Text-->
-											<div class="fs-6 text-gray-600 mb-5">Writing headlines for blog posts is as much an art as it is a science and probably warrants its own post, but for all advise is with what works for your great &amp; amazing audience.</div>
-											<!--end::Text-->
-											<!--begin::Alert-->
-											<!--begin::Notice-->
-											<div class="notice d-flex bg-light-warning rounded border-warning border border-dashed p-6">
-												<!--begin::Icon-->
-												<!--begin::Svg Icon | path: icons/duotune/general/gen044.svg-->
-												<span class="svg-icon svg-icon-2tx svg-icon-warning me-4">
-													<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-														<rect opacity="0.3" x="2" y="2" width="20" height="20" rx="10" fill="currentColor" />
-														<rect x="11" y="14" width="7" height="2" rx="1" transform="rotate(-90 11 14)" fill="currentColor" />
-														<rect x="11" y="17" width="2" height="2" rx="1" transform="rotate(-90 11 17)" fill="currentColor" />
-													</svg>
-												</span>
-												<!--end::Svg Icon-->
-												<!--end::Icon-->
-												<!--begin::Wrapper-->
-												<div class="d-flex flex-stack flex-grow-1">
-													<!--begin::Content-->
-													<div class="fw-bold">
-														<h4 class="text-gray-900 fw-bolder">We need your attention!</h4>
-														<div class="fs-6 text-gray-700">To start using great tools, please, please
-														<a href="#" class="fw-bolder">Create Team Platform</a></div>
-													</div>
-													<!--end::Content-->
-												</div>
-												<!--end::Wrapper-->
-											</div>
-											<!--end::Notice-->
-											<!--end::Alert-->
-										</div>
-										<!--end::Body-->
-									</div>
-									<!--end::Wrapper-->
-								</div>
+								@include('inc.auth.register-step-4')
 								<!--end::Step 5-->
 								<!--begin::Actions-->
 								<div class="d-flex flex-stack pt-15">
@@ -358,6 +307,22 @@ License: For each use you must have a valid license purchased only from above li
 		<!--end::Page Custom Javascript-->
 		<!--end::Javascript-->
 
+		<script>
+			$('.resendcode').slideUp();
+			// Element to indecate
+			var button = document.querySelector("#kt_button_1");
+
+			// Handle button click event
+			button.addEventListener("click", function() {
+				// Activate indicator
+				button.setAttribute("data-kt-indicator", "on");
+
+				// Disable indicator after 3 seconds
+				setTimeout(function() {
+					button.removeAttribute("data-kt-indicator");
+				}, 3000);
+			});
+		</script>
 	</body>
 	<!--end::Body-->
 </html>
