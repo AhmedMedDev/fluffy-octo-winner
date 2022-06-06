e445411d40b6415d3939133e922b914e41b342f2
unknown <dedoa82.as@gmail.com>
2022-06-06 05:12:14 +0200

    Build F*cken websockets env kit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -29,15 +29,16 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-import Echo from "laravel-echo"
+ import Echo from 'laravel-echo';
 
-window.Pusher = require('pusher-js');
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'your-pusher-key',
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: false,
-    disableStats: true,
-});
+ window.Pusher = require('pusher-js');
+ 
+ window.Echo = new Echo({
+     broadcaster: 'pusher',
+     key: "pusher_med_key",
+     cluster: "mtl",
+     wsHost: window.location.hostname,
+     wsPort: 6001,
+     forceTLS: false,
+     disableStats: true,
+ });
